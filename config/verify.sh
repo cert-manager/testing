8945a59723020ab47220ce76f824d073402b28b8
James Munnelly <james@munnelly.eu>
2018-08-31 01:05:30 +0100

    Enable strict mode on checkconfig

--- config/verify.sh
+++ config/verify.sh
@@ -21,6 +21,7 @@ set -o pipefail
 # This script is meant to be run via bazel with "bazel test //config:verify"
 
 ./config/checkconfig \
+    -strict \
     -config-path config/config.yaml \
     -job-config-path config/jobs \
     -plugin-config config/plugins.yaml
