cb8f1bc4fb20fcd9c8e68ccfb04bb985be004756
James Munnelly <james@munnelly.eu>
2019-04-08 01:28:18 +0100

    Add go image builder
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- WORKSPACE
+++ WORKSPACE
@@ -15,18 +15,22 @@ load("@bazel_skylib//:lib.bzl", "versions")
 
 versions.check(minimum_bazel_version = "0.15.0")
 
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
     name = "io_bazel_rules_go",
-    urls = ["https://github.com/bazelbuild/rules_go/releases/download/0.17.0/rules_go-0.17.0.tar.gz"],
-    sha256 = "492c3ac68ed9dcf527a07e6a1b2dcbf199c6bf8b35517951467ac32e421c06c1",
+    urls = ["https://github.com/bazelbuild/rules_go/releases/download/0.18.2/rules_go-0.18.2.tar.gz"],
+    sha256 = "31f959ecf3687f6e0bb9d01e1e7a7153367ecd82816c9c0ae149cd0e5a92bf8c",
 )
-
-load("@io_bazel_rules_go//go:deps.bzl", "go_register_toolchains", "go_rules_dependencies")
-
+http_archive(
+    name = "bazel_gazelle",
+    urls = ["https://github.com/bazelbuild/bazel-gazelle/releases/download/0.17.0/bazel-gazelle-0.17.0.tar.gz"],
+    sha256 = "3c681998538231a2d24d0c07ed5a7658cb72bfb5fd4bf9911157c0e9ac6a2687",
+)
+load("@io_bazel_rules_go//go:deps.bzl", "go_rules_dependencies", "go_register_toolchains")
 go_rules_dependencies()
-go_register_toolchains(
-    go_version = "1.11.5",
-)
+go_register_toolchains()
+load("@bazel_gazelle//:deps.bzl", "gazelle_dependencies", "go_repository")
+gazelle_dependencies()
 
 git_repository(
     name = "test_infra",
@@ -205,4 +209,12 @@ filegroup(
     visibility = ["//visibility:public"],
 )
 """,
+)
+
+go_repository(
+    name = "in_gopkg_yaml_v2",
+    commit = "5420a8b6744d3b0345ab293f6fcba19c978f1183",
+    remote = "https://github.com/go-yaml/yaml",
+    vcs = "git",
+    importpath = "gopkg.in/yaml.v2",
 )
---
+++ images/builder/main.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"sync"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+// builder builds docker images given a build.yaml file provided in the
+// images build directory.
+// It makes it easy to build a matrix of images, setting different build args
+// for each one.
+// It also handles templating image names and pushing docker images.
+// It is used by the testing repository to push testing docker images used in
+// ProwJobs.
+
+var (
+	confirm  bool
+	registry string
+	buildDir string
+)
+
+func init() {
+	flag.BoolVar(&confirm, "confirm", false, "set to true to confirm pushing images")
+	flag.StringVar(&registry, "registry", "eu.gcr.io/jetstack-build-infra-images", "docker image registry to push images to")
+	flag.StringVar(&buildDir, "build-dir", "", "path to a directory containing a build.yaml file")
+}
+
+func main() {
+	flag.Parse()
+
+	// validate flags
+	if errs := validateFlags(); len(errs) > 0 {
+		for _, err := range errs {
+			log.Printf(err.Error())
+		}
+		os.Exit(1)
+	}
+
+	if !confirm {
+		log.Printf("--confirm is set to false, not pushing images")
+	}
+
+	cfg, err := parseConfig(buildDir + "/build.yaml")
+	if err != nil {
+		log.Printf("error reading build.yaml: %v", err)
+		os.Exit(1)
+	}
+
+	ctxs, err := buildContexts(*cfg)
+	if err != nil {
+		log.Printf("error constructing build contexts: %v", err)
+		os.Exit(1)
+	}
+
+	for name, ctx := range ctxs {
+		log.Printf("building variant %q", name)
+		if err := ctx.Build(); err != nil {
+			log.Printf("error building variant %q: %v", name, err)
+			os.Exit(1)
+		}
+		log.Printf("built variant %q", name)
+	}
+
+	log.Printf("build all variants")
+	if !confirm {
+		log.Printf("skipping pushing images")
+		os.Exit(0)
+	}
+
+	for _, ctx := range ctxs {
+		for _, imgTmpl := range cfg.Images {
+			img := formatImageName(cfg, ctx, imgTmpl)
+			log.Printf("pushing image %q", img)
+			if err := ctx.Push(img); err != nil {
+				log.Printf("error pushing image %q: %v", img, err)
+				os.Exit(1)
+			}
+			log.Printf("pushed image %q", img)
+		}
+	}
+
+	log.Printf("SUCCESS")
+}
+
+func formatImageName(cfg *buildConfig, ctx *buildContext, tmpl string) string {
+	tmplMap := make(map[string]string)
+	for k, v := range ctx.BuildArgs {
+		tmplMap[k] = v
+	}
+	tmplMap["_NAME"] = cfg.Name
+	tmplMap["_REGISTRY"] = registry
+	tmplMap["_DATE_STAMP"] = "19700101"
+	tmplMap["_GIT_REF"] = "abcdefg"
+
+	img := tmpl
+	for k, v := range tmplMap {
+		img = strings.ReplaceAll(img, fmt.Sprintf("${%s}", k), v)
+	}
+
+	return img
+}
+
+func validateFlags() []error {
+	var errs []error
+	if buildDir == "" {
+		errs = append(errs, fmt.Errorf("build-dir must be specified"))
+	}
+	return errs
+}
+
+type buildConfig struct {
+	Name       string             `json:"name"`
+	Dockerfile string             `json:"dockerfile"`
+	Arguments  map[string]string  `json:"arguments"`
+	Variants   map[string]variant `json:"variants"`
+	Images     []string           `json:"images"`
+}
+
+type variant struct {
+	Arguments map[string]string `json:"arguments"`
+}
+
+func parseConfig(path string) (*buildConfig, error) {
+	d, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg buildConfig
+	if err := yaml.Unmarshal(d, &cfg); err != nil {
+		return nil, err
+	}
+
+	applyDefaults(&cfg)
+
+	if errs := validateConfig(cfg); len(errs) > 0 {
+		return nil, fmt.Errorf("config file error: %v", errs)
+	}
+
+	return &cfg, nil
+}
+
+func applyDefaults(cfg *buildConfig) {
+	if cfg.Dockerfile == "" {
+		cfg.Dockerfile = "Dockerfile"
+	}
+}
+
+func validateConfig(cfg buildConfig) []error {
+	var errs []error
+	if cfg.Name == "" {
+		errs = append(errs, fmt.Errorf("image field must be set"))
+	}
+	if cfg.Dockerfile == "" {
+		errs = append(errs, fmt.Errorf("dockerfile field must be set"))
+	}
+	return errs
+}
+
+// buildContexts constructs a slice of buildContexts for the given config
+// variations will be expanded in this function.
+func buildContexts(cfg buildConfig) (map[string]*buildContext, error) {
+	if len(cfg.Variants) == 0 {
+		ctx := constructContext(cfg, nil)
+		return map[string]*buildContext{"": ctx}, nil
+	}
+
+	ctxs := make(map[string]*buildContext)
+	for name, v := range cfg.Variants {
+		ctx := constructContext(cfg, v.Arguments)
+		ctxs[name] = ctx
+	}
+
+	return ctxs, nil
+}
+
+func constructContext(cfg buildConfig, extraArgs map[string]string) *buildContext {
+	ctx := buildContext{
+		Dockerfile: cfg.Dockerfile,
+		Directory:  buildDir,
+	}
+	buildArgs := make(map[string]string)
+	for k, v := range cfg.Arguments {
+		buildArgs[k] = v
+	}
+	for k, v := range extraArgs {
+		buildArgs[k] = v
+	}
+	ctx.BuildArgs = buildArgs
+	return &ctx
+}
+
+// buildContext provides an abstraction to build docker images using different
+// docker build systems.
+// Initially only docker is supported.
+type buildContext struct {
+	Dockerfile string
+	Directory  string
+	BuildArgs  map[string]string
+
+	name  string
+	built bool
+
+	nameLock  sync.Mutex
+	buildLock sync.Mutex
+}
+
+// Build will build the docker image given the context config
+func (b *buildContext) Build() error {
+	b.buildLock.Lock()
+	defer b.buildLock.Unlock()
+	if b.built {
+		return nil
+	}
+
+	log.Printf("building docker image dockerfile=%s, directory=%s, buildArgs=%v", b.Dockerfile, b.Directory, b.BuildArgs)
+	args := b.buildCmd()
+	if err := b.runDocker(args...); err != nil {
+		return err
+	}
+	log.Printf("built docker image")
+	b.built = true
+	return nil
+}
+
+func (b *buildContext) buildCmd() []string {
+	args := []string{"build", "-t", b.temporaryImageName(), "-f", path.Join(b.Directory, b.Dockerfile)}
+	for k, v := range b.BuildArgs {
+		args = append(args, "--build-arg", k+"="+v)
+	}
+	args = append(args, b.Directory)
+	return args
+}
+
+func (b *buildContext) temporaryImageName() string {
+	b.nameLock.Lock()
+	defer b.nameLock.Unlock()
+
+	if b.name == "" {
+		b.name = randString(16)
+	}
+
+	return "builder:" + b.name
+}
+
+// Push will push the docker image that has been built with the image name
+// provided.
+// If Build has not been called, the image will be built.
+// It is safe to call this function multiple times in parallel.
+func (b *buildContext) Push(name string) error {
+	if err := b.Build(); err != nil {
+		return err
+	}
+
+	if err := b.runDocker("tag", b.temporaryImageName(), name); err != nil {
+		return err
+	}
+
+	if err := b.runDocker("push", name); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *buildContext) runDocker(args ...string) error {
+	log.Printf("running with args %v", args)
+	cmd := exec.Command("docker", args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
