d3d665ae530bcbc2e275aa083f9176d6fca8ae22
Ashley Davis <ashley.davis@venafi.com>
2024-04-18 19:37:50 +0100

    Change periodic cert-manager jobs to use cron
    This spreads the jobs pretty well throughout the day to try and minimise
    the amount of compute we're using for any one instant of cert-manager
    tests.
    
    This algorithm seems to give reasonable performance in spread without
    spending too much time digging into making it perfect.
    
    Signed-off-by: Ashley Davis <ashley.davis@venafi.com>

--- config/prowgen/pkg/context.go
+++ config/prowgen/pkg/context.go
@@ -19,8 +19,7 @@ package pkg
 
 import (
 	"fmt"
-	"strconv"
-	"time"
+	"math"
 )
 
 // ProwContext holds jobs and information required to configure jobs for a given release channel.
@@ -51,8 +50,6 @@ type ProwContext struct {
 
 	presubmits []*PresubmitJob
 	periodics  []*PeriodicJob
-
-	minutesCounter time.Time
 }
 
 // RequiredPresubmit adds a presubmit which is run by default and required to pass before a PR can be merged
@@ -116,17 +113,74 @@ func (pc *ProwContext) Periodics(job *Job, periodicityHours int) {
 				BaseRef: pc.Branch,
 			},
 		},
-		Interval: strconv.Itoa(periodicityHours) + "h",
-		// TODO: use Cron instead of Interval
-		// Cron: pc.cronSchedule(periodicityHours),
+		PeriodicityHours: periodicityHours,
+		// Use a minute and startHour of 0 when adding the period here, but in JobFile
+		// when we actually generate the JobFile struct we'll recalculate values
+		// to spread the periodics evenly across every hour / day
+		Cron: cronSchedule(0, 0, periodicityHours),
 	})
 }
 
 func (pc *ProwContext) JobFile() *JobFile {
-	// TODO: when using Cron instead of Interval for periodics, adjust all periodics
-	// here to spread them evenly throughout the hour
-
 	presubmitKey := fmt.Sprintf("%s/%s", pc.Org, pc.Repo)
+
+	// By dividing 60 by the number of periodics we get the maximum number of minutes
+	// apart which we can schedule jobs, in an effort to maximise the amount of time
+	// that each job could theoretically run without anything else running in parallel.
+
+	// We aim to maximise the amount of CPU available to each job at startup, which is
+	// often when most jobs need a lot of CPU (e.g. to set up tests / clusters)
+	// We use ceil because more spreading isn't a bad thing
+	minuteSpread := int(math.Ceil(60.0 / float64(len(pc.periodics))))
+
+	// Count the number of jobs with each periodicity to make it easier to spread them later
+	periodicityCounts := map[int]int{}
+
+	for _, p := range pc.periodics {
+		periodicityCounts[p.PeriodicityHours] += 1
+	}
+
+	// periodicitySeen is used to track how many times we've seen a job with a given
+	// periodicity, which allows us to spread jobs with the same periodicity across the day
+	periodicitySeen := map[int]int{}
+
+	for i, p := range pc.periodics {
+		minute := (i * minuteSpread) % 60
+
+		// hourCounter is how many jobs with the same periodicity we've already seen
+		hourCounter := periodicitySeen[p.PeriodicityHours]
+
+		periodicitySeen[p.PeriodicityHours] = hourCounter + 1
+
+		// Hour spread is how far apart we can place jobs in starting hours
+		// E.g. Say we have 5 jobs with periodicity 4.
+		// (Bear in mind these jobs can only start in hours 0,1,2,3 [1])
+		// ceil(4/5) = 1, meaning we have to place a job every hour and can't space them out any more.
+
+		// If we instead had 3 jobs with periodicity 8, ceil(8/3) = 3 and so we can place jobs at
+		// 0,3,6 (or 1,4,7) to spread them out more.
+
+		// [1] Effectively we operate modulo the periodicity; if (start hour >= periodicity) it
+		// reduces the number of invocations possible in a calendar day, e.g.:
+		// "0 7-23/8 * * *" runs 3 times in a day (at 07:00, 15:00 and 23:00), but
+		// "0 8-23/8 * * *" only runs twice (at 08:00 and 16:00)
+		hourSpread := int(math.Floor(float64(p.PeriodicityHours) / float64(periodicityCounts[p.PeriodicityHours])))
+		if hourSpread == 0 {
+			hourSpread = 1
+		}
+
+		if p.PeriodicityHours == 24 {
+			// 24h periodicity is different because it can be started at any hour and will always be run
+			// once per day
+			// In this case, just set the spread to 7 which gives a pretty good spread modulo 24:
+			// [0, 7, 14, 21, 4, 11, 18, 1, 8, 15, 22, 5, 12, 19, 2, 9, 16, 23, 6, 13, 20, 3, 10, 17]
+			hourSpread = 7
+		}
+
+		startHour := (hourSpread * hourCounter) % p.PeriodicityHours
+
+		p.Cron = cronSchedule(minute, startHour, p.PeriodicityHours)
+	}
 
 	return &JobFile{
 		Presubmits: map[string][]*PresubmitJob{
@@ -156,19 +210,6 @@ func (pc *ProwContext) periodicDashboardName() string {
 	return fmt.Sprintf("%s-periodics-%s", pc.Repo, pc.Branch)
 }
 
-func (pc *ProwContext) cronSchedule(periodicityHours int) string {
-	minute := pc.minutesValue()
-
-	return fmt.Sprintf("*/%d %d * * *", minute, periodicityHours)
-}
-
-// minutesValue returns a minute value (0 - 59) at which a test should be run and then
-// increases the next value returned. This helps to prevent every test running at the same
-// minute within the hour causing a spiky distribution of tests.
-func (pc *ProwContext) minutesValue() int {
-	minuteVal := pc.minutesCounter.Minute()
-
-	pc.minutesCounter = pc.minutesCounter.Add(4 * time.Minute)
-
-	return minuteVal
+func cronSchedule(minute int, startHour int, periodicityHours int) string {
+	return fmt.Sprintf("%02d %02d-23/%02d * * *", minute, startHour, periodicityHours)
 }
