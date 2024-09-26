3f66efa13253d0ab7ad890bfc26f4b504de6ace8
Richard Wall <richard.wall@venafi.com>
2024-09-26 14:16:47 +0100

    Add release-1.16 to the prowgen specs
    Using the same Kubernetes versions as for master
    
    Signed-off-by: Richard Wall <richard.wall@venafi.com>

--- config/prowgen/prowspecs/specs.go
+++ config/prowgen/prowspecs/specs.go
@@ -114,6 +114,27 @@ var knownBranches map[string]BranchSpec = map[string]BranchSpec{
 		e2eCPURequest:    "7000m",
 		e2eMemoryRequest: "6Gi",
 	},
+	"release-1.16": {
+		prowContext: &pkg.ProwContext{
+			Branch: "release-1.16",
+
+			// Use latest image.
+			Image: pkg.CommonTestImage,
+
+			// NB: we don't use a presubmit dashboard outside of "master", currently
+			PresubmitDashboard: false,
+			PeriodicDashboard:  true,
+
+			Org:  "cert-manager",
+			Repo: "cert-manager",
+		},
+
+		primaryKubernetesVersion: "1.31",
+		otherKubernetesVersions:  []string{"1.27", "1.28", "1.29", "1.30"},
+
+		e2eCPURequest:    "7000m",
+		e2eMemoryRequest: "6Gi",
+	},
 	"master": {
 		prowContext: &pkg.ProwContext{
 			Branch: "master",
